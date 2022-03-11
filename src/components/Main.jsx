9b562e30f923589c025700de6a2ac88375ec72a3
Wyatt Wilson <wyattwilson916@gmail.com>
2022-03-11 14:18:35 -0500

    Firt Commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Navbar } from "./index";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 const Main = () => {
   return (
-    <div>
-      <h1>hello main</h1>
+    <div className="main_container">
+      <Navbar />
+      <h1 className="main_title">Hello World!!!</h1>
+      <Switch>
+        <Route
+          path="/"
+          render={()=>{
+            return; //component to render;
+          }}
+        />
+        <Redirect to="/" />
+      </Switch>
     </div>
   );
 };
